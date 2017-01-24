57a2d37926491d25509044fab78e8715e3ccd981
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-01-24 22:03:22 +0100

    restructured

--- src/main/java/de/lgohlke/utils/FdupesApplication.java
+++ src/main/java/de/lgohlke/utils/FdupesApplication.java
@@ -5,43 +5,65 @@ import de.lgohlke.utils.filter.map.MapFilter;
 import de.lgohlke.utils.filter.map.NotSameFilesystemFilter;
 import de.lgohlke.utils.filter.map.SingleSizeFilter;
 import de.lgohlke.utils.filter.map.TooSmallFilter;
-import de.lgohlke.utils.filter.pair.NotSameOwnerFilter;
-import de.lgohlke.utils.filter.pair.Pair;
-import de.lgohlke.utils.filter.pair.PairFilter;
-import de.lgohlke.utils.filter.pair.SameFileFilter;
+import de.lgohlke.utils.filter.pair.*;
+import de.lgohlke.utils.status.Progress;
+import de.lgohlke.utils.status.SizeFormatter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
+@RequiredArgsConstructor
 public class FdupesApplication {
+    private final String _path;
 
-    public static void main(String[] args) throws Exception {
-        Path path = Paths.get("/backup/backintime/lars-MS-7930/lars/4");
-        log.info("scan {}", path);
+    private Path path;
+    private Progress progress;
+    private Map<Long, List<Path>> sizeToFileMap;
 
-        // only for profiling
-        TimeUnit.SECONDS.sleep(10);
-
-        Map<Long, List<Path>> sizeToFileMap = DirWalker.walk(path);
+    FdupesApplication init() throws IOException {
+        path = Paths.get(_path);
+        log.info("scanning {}", path);
+        sizeToFileMap = DirWalker.walk(path);
         log.info("scanned");
-        List<MapFilter> filters = Lists.newArrayList(new SingleSizeFilter(), new TooSmallFilter(), new NotSameFilesystemFilter(path));
+        progress = new Progress(sumFilesize(sizeToFileMap));
+        return this;
+    }
+
+    FdupesApplication filterGlobal() {
+        MapFilter tooBigFilter = sizeToFileMap -> sizeToFileMap.entrySet()
+                                                               .stream()
+                                                               .filter(entry -> entry.getKey() < 4 * 1024 * 1024)
+                                                               .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        List<MapFilter> filters = Lists.newArrayList(new SingleSizeFilter(), new TooSmallFilter(), new NotSameFilesystemFilter(path), tooBigFilter);
 
         for (MapFilter filter : filters) {
-            int oldSize = sizeToFileMap.size();
-            log.info("running {}", filter.getClass().getSimpleName());
-            sizeToFileMap = filter.filter(sizeToFileMap);
-            int newSize = sizeToFileMap.size();
-            log.info(" filtered from {} -> {}", oldSize, newSize);
+            long oldSize = sumFilesize(sizeToFileMap);
+            if (oldSize > 0) {
+                log.info("running {}", filter.getClass().getSimpleName());
+                sizeToFileMap = filter.filter(sizeToFileMap);
+                long newSize = sumFilesize(sizeToFileMap);
+                log.info(" filtered from {} -> {}", size(oldSize), size(newSize));
+                progress.reduce(oldSize - newSize);
+            }
+            log.info(progress.status());
         }
+        return this;
+    }
 
-        List<PairFilter> pairFilters = Lists.newArrayList(new NotSameOwnerFilter(), new SameFileFilter());
+    FdupesApplication filterCandidatePairs(Function<Pair, Void> eleminateDuplicateOperation) {
+        List<PairFilter> pairFilters = Lists.newArrayList(new SameFileFilter(), new NotSameOwnerFilter(), new SameHashFilter());
 
         log.info("start pairfiltering");
         sizeToFileMap.entrySet().stream().sorted(Comparator.comparing(Map.Entry::getKey)).forEach(entry -> {
@@ -50,10 +72,55 @@ public class FdupesApplication {
 
             for (PairFilter pairFilter : pairFilters) {
                 int oldSize = pairs.size();
-                pairs = pairs.stream().filter(pairFilter::select).collect(Collectors.toList());
-                log.info("  filtered {} -> {} with {}", oldSize, pairs.size(), pairFilter.getClass().getSimpleName());
+                if (oldSize > 0) {
+                    pairs = pairs.stream().filter(pairFilter::select).collect(Collectors.toList());
+                    log.info("  filtered {} -> {} with {}", oldSize, pairs.size(), pairFilter.getClass()
+                                                                                             .getSimpleName());
+                }
             }
+
+            pairs.stream().map(eleminateDuplicateOperation);
+
+            int minus = pairs.stream()
+                             .flatMap(pair -> Stream.of(pair.getP1(), pair.getP2()))
+                             .collect(Collectors.toSet())
+                             .size();
+
+            progress.reduce(entry.getValue().size() - minus);
+            log.info(progress.status());
         });
+        return this;
+    }
+
+    private static String size(int oldSize) {
+        return size((long) oldSize);
+    }
+
+    private static String size(long oldSize) {
+        return SizeFormatter.readableFileSize(oldSize);
+    }
+
+    private static long sumFilesize(Map<Long, List<Path>> sizeToFileMap) {
+        return sizeToFileMap.entrySet()
+                            .stream()
+                            .map(entry -> entry.getKey() * entry.getValue().size())
+                            .mapToLong(Long::longValue)
+                            .sum();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String pathToScan = "/backup/backintime/lars-MS-7930/lars/4";
+
+        // only for profiling
+        TimeUnit.SECONDS.sleep(10);
+
+        new FdupesApplication(pathToScan).init().filterGlobal().filterCandidatePairs(pair -> {
+            System.out.println("duplicates: " + pair.getP1() + " & " + pair.getP2());
+            return null;
+        });
+
+
         log.info("finish");
     }
+
 }
