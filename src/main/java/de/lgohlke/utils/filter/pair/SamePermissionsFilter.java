a44e66c6089ce561a33bba2df5a099c3eeb5bf31
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-04-19 00:19:58 +0200

    adds Permissions Cache

--- src/main/java/de/lgohlke/utils/filter/pair/SamePermissionsFilter.java
+++ src/main/java/de/lgohlke/utils/filter/pair/SamePermissionsFilter.java
@@ -1,21 +1,32 @@
 package de.lgohlke.utils.filter.pair;
 
-import java.io.IOException;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class SamePermissionsFilter implements PairFilter {
+    private final static Cache<Path, Set<PosixFilePermission>>
+            CACHE = CacheBuilder.newBuilder().maximumSize(10000).build();
+
     @Override
     public boolean select(Pair pair) {
         try {
-            Set<PosixFilePermission> filePermissionsA = Files.getPosixFilePermissions(pair.getP1(), LinkOption.NOFOLLOW_LINKS);
-            Set<PosixFilePermission> permissionsB = Files.getPosixFilePermissions(pair.getP2(), LinkOption.NOFOLLOW_LINKS);
+            Set<PosixFilePermission> permissionsA = retrievePerms(pair.getP1());
+            Set<PosixFilePermission> permissionsB = retrievePerms(pair.getP2());
 
-            return filePermissionsA.equals(permissionsB);
-        } catch (IOException e) {
+            return permissionsA.equals(permissionsB);
+        } catch (ExecutionException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private static Set<PosixFilePermission> retrievePerms(Path path) throws ExecutionException {
+        return CACHE.get(path, () -> Files.getPosixFilePermissions(path, LinkOption.NOFOLLOW_LINKS));
     }
 }
